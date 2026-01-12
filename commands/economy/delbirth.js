b76ab3cc8e32e43df8c99311f9e64aa7d7471f12
𝕸𝖆𝖙𝖍𝖊𝖔 <38385526+MatheoDark@users.noreply.github.com>
2026-01-12 18:34:03 -0300

    Update delbirth.js

--- commands/economy/delbirth.js
+++ commands/economy/delbirth.js
@@ -1,8 +1,14 @@
+import { resolveLidToRealJid } from "../../lib/utils.js"
+
 export default {
-  command: ['delbirth'],
+  command: ['delbirth', 'borrarcumple', 'delcumple'],
   category: 'rpg',
-  run: async ({client, m}) => {
-    const user = global.db.data.users[m.sender]
+  run: async ({ client, m }) => {
+    const userId = await resolveLidToRealJid(m.sender, client, m.chat);
+    const user = global.db.data.users[userId]
+
+    if (!user) return m.reply(`《✧》 Perfil no encontrado.`)
+
     if (!user.birth) return m.reply(`《✧》 No tienes una fecha de nacimiento establecida.`)
 
     user.birth = ''
