e9a2f77e3fb7dcf0aa7338e6fa5496067c5bfb99
𝕸𝖆𝖙𝖍𝖊𝖔 <38385526+MatheoDark@users.noreply.github.com>
2026-01-12 18:45:18 -0300

    Change command category and update response messages

--- commands/economy/setdesc.js
+++ commands/economy/setdesc.js
@@ -1,21 +1,19 @@
+import { resolveLidToRealJid } from "../../lib/utils.js"
+
 export default {
   command: ['setdescription', 'setdesc'],
-  category: 'rpg',
-  run: async ({client, m, args}) => {
-    const user = global.db.data.users[m.sender]
+  category: 'profile',
+  run: async ({ client, m, args, usedPrefix }) => {
+    const prefa = usedPrefix || '/'
+    const userId = await resolveLidToRealJid(m.sender, client, m.chat);
+    const user = global.db.data.users[userId]
+
+    if (user.description) return m.reply(`《✧》 Ya tienes descripción. Usa *${prefa}deldesc* para borrarla.`)
+
     const input = args.join(' ')
-
-    if (user.description)
-      return m.reply(
-        `《✧》 Ya tienes una descripción. Usa › *${prefa}deldescription* para eliminarla.`,
-      )
-
-    if (!input)
-      return m.reply(
-        '《✧》 Debes especificar una descripción válida.',
-      )
+    if (!input) return m.reply('《✧》 Escribe tu descripción.')
 
     user.description = input
-    return m.reply(`✎ Se ha establecido tu descripción:\n> *${user.description}*`)
+    return m.reply(`✎ Descripción actualizada:\n> *${user.description}*`)
   },
 };
