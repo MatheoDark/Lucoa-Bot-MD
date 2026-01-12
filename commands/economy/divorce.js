bc8bd6f95e4899419625741b558df36e78e571f2
𝕸𝖆𝖙𝖍𝖊𝖔 <38385526+MatheoDark@users.noreply.github.com>
2026-01-12 18:33:33 -0300

    Update divorce.js

--- commands/economy/divorce.js
+++ commands/economy/divorce.js
@@ -1,18 +1,33 @@
+import { resolveLidToRealJid } from "../../lib/utils.js"
+
 export default {
-  command: ['divorce'],
+  command: ['divorce', 'divorciarse'],
   category: 'rpg',
-  run: async ({client, m}) => {
-    const db = global.db.data
-    const userId = m.sender
-    const partnerId = db.users[userId]?.marry
+  run: async ({ client, m }) => {
+    
+    // Resolver tu ID real
+    const userId = await resolveLidToRealJid(m.sender, client, m.chat);
+    const user = global.db.data.users[userId]
 
-    if (!partnerId) return m.reply('《✧》 Tú no estás casado con nadie.')
+    if (!user || !user.marry) return m.reply('《✧》 No estás casado con nadie... disfruta tu libertad. 🍃')
 
-    db.users[userId].marry = ''
-    db.users[partnerId].marry = ''
+    const partnerId = user.marry
+
+    // Limpiar tu estado
+    user.marry = ''
+    
+    // Limpiar el estado de la pareja (si existe en la DB)
+    const partner = global.db.data.users[partnerId]
+    if (partner) {
+        partner.marry = ''
+    }
+
+    // Nombres para el mensaje
+    const myName = user.name || userId.split('@')[0]
+    const partnerName = partner ? (partner.name || partnerId.split('@')[0]) : 'tu ex-pareja'
 
     return m.reply(
-      `✎ *${db.users[userId]?.name || userId.split('@')[0]}* te has divorciado de *${db.users[partnerId]?.name || partnerId.split('@')[0]}*.`,
+      `💔 *DIVORCIO FINALIZADO*\n\n📝 *${myName}* ha firmado los papeles.\nTe has divorciado de *${partnerName}*.\n\n_Ahora eres libre nuevamente._`
     )
   },
 };
